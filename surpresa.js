302237ef7aa1bfde186dc6546151c5257fac37cd
Lucas Carneiro <dsousalucas2005@gmail.com>
2026-03-11 23:48:44 -0300

    update letter
    atualizando conteúdo da carta

--- surpresa.js
+++ surpresa.js
@@ -1,4 +1,10 @@
-const texto = "Minha querida, cada dia ao seu lado é uma nova descoberta. Fiz esse site para mostrar que, assim como no código, nosso amor tem uma lógica perfeita: você me completa. Obrigado por ser minha parceira de vida...";
+const texto = " Oi, amor! Você sabe que não sou de escrever, mas irei tentar. Nem sei por onde começar. Porém vamos lá. \
+Depois que te conheci, minha visão de mundo mudou, e não consigo conceber um mundo em que o nós não exista. Sei que estamos passando pelo nosso momento mais difícil, é aquela fase que a paixão deixa de cegar a gente e você consegue ver claramente todos os meus defeitos, que eu ainda não sou aquele homem que você idealizou.\
+Vou falar um pouco sobre o que eu sinto. Quando te vejo meu coração não acelera, pelo contrário, todos os músculos do meu ser relaxam, se descontraem, porque sei que estou em um lugar seguro. Meus olhos riem a toa só de pensar em você.\
+Quero que você se sinta confortável comigo perto  e distante, que não precise temer nada, que se sinta cuidada, respeitada, amada. Que no futuro encaremos todas as dificuldades do nosso relacionamento à distância como uma grande prova que fortaleceu o nosso amor.\
+Percebeu que em nenhuma parte dessa carta eu disse diretamente te amo, porque estou aprendendo que o amor não é apenas um sentimento de afeto, o amor é o que o amor faz. Tentei falar nessa carta como te amo sem precisar usar essa palavra. E prometo que vou te amar de todas as maneiras possíveis.\
+";
+
 
 let index = 0;
 const velocidade = 50; // milissegundos por letra
